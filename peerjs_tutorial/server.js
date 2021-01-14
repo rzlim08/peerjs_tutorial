dc9370ca93f98973ea2a276499926a6f91267f2b
Ryan Lim <rzlim08@gmail.com>
2021-01-13 19:21:30 -0500

    add ssl and audio streaming code

--- peerjs_tutorial/server.js
+++ peerjs_tutorial/server.js
@@ -1,17 +1,25 @@
 //Derived from: https://medium.com/samsung-internet-dev/building-an-internet-connected-phone-with-peerjs-775bd6ffebec
 const express = require("express");
-const http = require('http');
+const https = require('https');
 const path = require('path');
 const app = express();
-const server = http.createServer(app);
+var fs     = require('fs');
+var options = {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem")
+};
+const server = https.createServer(options, app);
 
 const { ExpressPeerServer } = require('peer');
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || "443";
 
 const peerServer = ExpressPeerServer(server, {
     debug: true,
     path: '/myapp',
-    ssl: {}
+    secure:true,
+    ssl: {
+        options
+    }
 });
 
 app.use(peerServer);
